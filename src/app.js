f17ecebd754fd78d3bcd2de63af4938267c1de94
PushpamSingh <pushpamsingh204@gmail.com>
2025-07-08 15:20:56 +0530

    add s file vercel.json

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ export const app=express();
 
 app.use(cors(
     {
-        origin:process.env.CORS_ORIGIN,
+        origin:[process.env.CORS_ORIGIN],
         credentials:true,
         optionsSuccessStatus: 200,
     }

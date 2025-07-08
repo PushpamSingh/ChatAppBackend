e757176731cba9ff6a332c1c487fc9a418a759b3
PushpamSingh <pushpamsingh204@gmail.com>
2025-07-08 11:19:05 +0530

    feat: Configure Vercel deployment and update API proxy URL in Vite config

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(cors(
         optionsSuccessStatus: 200,
     }
 ))
-
+app.set('trust proxy', 1); // trust first proxy
 app.use(express.json("limit:16kb"));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));

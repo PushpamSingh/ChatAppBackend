9931be9d7879e5d3de6f87690fcfa1da0be6f240
PushpamSingh <pushpamsingh204@gmail.com>
2025-07-09 11:43:56 +0530

    fix(cors): remove localhost and wildcard origins from CORS configuration

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ export const app=express();
 
 app.use(cors(
     {
-        origin:[process.env.CORS_ORIGIN1, process.env.CORS_ORIGIN2, process.env.CORS_ORIGIN3,"http://localhost:5173","*"],
+        origin:[process.env.CORS_ORIGIN1, process.env.CORS_ORIGIN2, process.env.CORS_ORIGIN3],
         credentials:true,
         optionsSuccessStatus: 200,
     }
